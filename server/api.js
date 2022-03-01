a531132cf7709c320f9ee941bdeb6d870646452c
Logesh T <53284256+Logesh18@users.noreply.github.com>
2022-03-01 12:50:27 +0530

    Update api.js

--- server/api.js
+++ server/api.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
 const bodyparser = require('body-parser');
-// const mysql = require('mysql2');
 const cors = require('cors');
 const port=8000;
 
@@ -9,29 +8,18 @@ app.use(cors({ origin: "*" }));
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({extended: true}));
 
-// const connection=mysql.createConnection({
-//     host:'sql6.freesqldatabase.com',
-//     user:'sql6474167',
-//     password:'hby1UWFzJt',
-//     database:'sql6474167',
-//     port:3306
-// });
 
 var mysql = require('mysql');
 var connection = mysql.createConnection({
   host: 'SG-mysql1-5763-mysql-master.servers.mongodirector.com',
   user: 'sgroot',
-  password: '7&VhKXTLCZK4RNI5',
+  password: '',
   database:'node_project',
   port: 3306,  
 });
 connection.connect(function(err) {
     if (err) {throw err;}
     console.log('Connected');
-    // connection.query("SELECT * FROM users", function (err, result, fields) {
-    //     if (err) throw err;
-    //     console.log(result);
-    // });
 });
 
 try
